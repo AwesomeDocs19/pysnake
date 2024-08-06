4c2e8579878ccf58874ed89c21e58639e2e03bf5
e <e@e.e>
2024-08-06 15:52:07 -0700

    add food

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 pygame.init()
 
 # Display settings
@@ -12,6 +13,9 @@ x1 = dis_width / 2
 y1 = dis_height / 2
 x1_change = 0
 y1_change = 0
+
+foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
 
 # Main loop
 running = True
@@ -37,6 +41,7 @@ while running:
     y1 += y1_change
     dis.fill((0, 0, 0))
     pygame.draw.rect(dis, (255, 255, 255), [x1, y1, snake_block, snake_block])
+    pygame.draw.rect(dis, (0, 255, 0), [foodx, foody, snake_block, snake_block])
     pygame.display.update()
 
 pygame.quit()
