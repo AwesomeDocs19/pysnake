fe82ab05c86153e349843cc1196cfa71b65f0a72
e <e@e.e>
2024-08-06 15:51:03 -0700

    add Snake and movement

--- game.py
+++ game.py
@@ -7,13 +7,36 @@ dis_height = 600
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game')
 
+snake_block = 10
+x1 = dis_width / 2
+y1 = dis_height / 2
+x1_change = 0
+y1_change = 0
+
+# Main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -snake_block
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = snake_block
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -snake_block
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = snake_block
+                x1_change = 0
 
+    x1 += x1_change
+    y1 += y1_change
     dis.fill((0, 0, 0))
+    pygame.draw.rect(dis, (255, 255, 255), [x1, y1, snake_block, snake_block])
     pygame.display.update()
 
 pygame.quit()
